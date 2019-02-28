b6de71265d989520423fbba81233f27fbab54443
Derek <derekdenhartigh@gmail.com>
2019-02-28 15:18:31 -0500

    added exercise guidelines to main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 "use strict";
+/*
+Create a circle that follows the mouse around the page. 
+Whenever the mouse moves, the circle  immediately jumps to the new location.    
+Hint: The event parameter to the event handler provides information on the location of the  
+mouse.    
+
+Extended Challenges:  
+● Create a trail behind the mouse as it moves.  
+● Instead of the circle immediately jumping to the new mouse location, have it slowly  
+make progress toward the cursor.   
+*/
